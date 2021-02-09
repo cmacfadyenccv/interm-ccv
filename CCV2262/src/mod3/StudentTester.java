19287f5dd64dcea42eeafc52f3ab88c0c8873ecc
cmacfadyenccv <41872980+cmacfadyenccv@users.noreply.github.com>
2021-02-09 00:27:54 -0500

    Update and rename CCV2262/src/StudentTester.java to CCV2262/src/mod3/StudentTester.java

--- CCV2262/src/mod3/StudentTester.java
+++ CCV2262/src/mod3/StudentTester.java
@@ -14,21 +14,21 @@ public class StudentTester {
 		Math.max(9, 7);
 		System.out.println(CCVstudent.MAX_NAME_LENGTH);
 
-//		d.setName("Olive Oyl");
-//		System.out.println(c.getName());
-//
-//		System.out.println(c);
-//		System.out.println(d);
-//
-//		System.out.println("The difference in id's is: " + c.compareTo(d));
-//		System.out.println("The difference in id's is: " + d.compareTo(c));
-//
-//		Classroom classroom = new Classroom(c);
-//		System.out.println(classroom.getClassPresident());
-//
-//		classroom.increaseId(d);
-//		System.out.println("The id of the object that was passed is " +
-//				d.getId());
+		d.setName("Olive Oyl");
+		System.out.println(c.getName());
+
+		System.out.println(c);
+		System.out.println(d);
+
+		System.out.println("The difference in id's is: " + c.compareTo(d));
+		System.out.println("The difference in id's is: " + d.compareTo(c));
+
+		Classroom classroom = new Classroom(c);
+		System.out.println(classroom.getClassPresident());
+
+		classroom.increaseId(d);
+		System.out.println("The id of the object that was passed is " +
+				d.getId());
 
 
 	}
