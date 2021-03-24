7fd2a7301434760573e758b894d1a2454a26b7f9
cmacfadyenccv <41872980+cmacfadyenccv@users.noreply.github.com>
2021-03-24 17:13:04 -0400

    Add files via upload

---
+++ CCV2262/src/InterfaceDemo.java
@@ -0,0 +1,15 @@
+package mod4;
+
+public interface InterfaceDemo {
+
+	//data
+	public final int maxVolume = 127;
+	
+	//abstract methods
+	public void knob1Increase(double amtInc);
+	public void knob1Decrease(double amtDec); 
+	public String pad1Pressed(double velocity);
+	public void pad1Released();
+	public String progSelectPushed();
+	
+}
